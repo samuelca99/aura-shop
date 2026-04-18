0a94a713d6512b1ce98bdae29e498012e012663c
Samuel <samuelchoque55@gmail.com>
2026-04-18 08:22:20 -0400

    Forzar modo SSR en Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // <-- AÑADE ESTA LÍNEA (no olvides la coma al final)
   adapter: vercel()
 });
